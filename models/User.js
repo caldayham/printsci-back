f3cf5200d86fa904c2ab3c6e327f1cf1a1eea6ed
caldayham <caldayham@gmail.com>
2022-09-13 09:00:53 -0700

    added more items to the user object

--- models/User.js
+++ models/User.js
@@ -4,12 +4,31 @@ const UserSchema = new mongoose.Schema(
     {
         username: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
+        organization: {type: String},
+        selfDescription: {type: String},
+        img: {type: String},
+        hashedSaltedPass: { type: String, required: true },
+        salt: {type: String, required: true},
+        profiles: [
+            {
+                firstName: {type: String},
+                lastName: {type: String},
+                role: {type: String},
+            }
+        ],
+        cart: [
+            {
+                productId: {type: String},
+                quantity: {type: Number}
+            }
+        ],
+        orderIds: {type: Array},
+        reviewIds: {type: Array },
+        questionIds: {type: Array},
         isAdmin: {
             type: Boolean,
             default: false,
         },
-        img: {type: String},
     },
     { timestamps: true }
 );
